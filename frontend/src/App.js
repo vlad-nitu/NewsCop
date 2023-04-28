4f9123cc74463d0238af4125c070d14b89dadd28
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2023-04-28 11:05:08 +0200

    Import axios in React to connect it to Django to make HTTP requests
    See this YT tutorial: https://www.youtube.com/watch\?v\=fBA-jaWab9k that is based on this GeeksForGeeks blogpost: https://www.geeksforgeeks.org/how-to-connect-django-with-reactjs/

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+
+  state = { details: [], }
+  componentDidMount() {
+    let data;
+    axios.get('http://localhost:8000')
+    .then(res => {
+        data = res.data;
+        this.setState({
+          details: data
+        });
+      })
+    .catch(err => console.log('Error ${err} occured when GET was called'));
+  }
+
+  render() { 
+    return (
       <div>
-        <h1> News articles overlap </h1>
+        <header> Data generated from Django </header>
+        {this.state.details.map((output, id) => 
+          (<div key={id}> 
+            <div>
+              <h2> {output.news} </h2>
+              <h3> {output.detail} </h3>
+            </div>
+          </div>)
+        )} 
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
