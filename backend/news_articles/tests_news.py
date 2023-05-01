2b21087e080178b00aa8f294e45491c711c0e9c5
Rares Toader <r.a.toader@student.tudelft.nl>
2023-05-01 23:32:40 +0200

    Few updates for checkstyle pass

--- backend/news_articles/tests_news.py
+++ backend/news_articles/tests_news.py
@@ -3,5 +3,5 @@ from django.test import TestCase
 # Create your tests here.
 class MainTest(TestCase):
     def test_check_running(self):
-          self.assertTrue(True)
+        self.assertTrue(True)
 
