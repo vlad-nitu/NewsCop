cb6e65d153f32aad25d15d456cda6fdbab3a7c36
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2023-05-01 14:40:26 +0200

    created two separate yml files and merged in the root

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,25 @@
-import axios from 'axios';
-import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends React.Component {
-
-  state = { details: [], }
-  componentDidMount() {
-    let data;
-    axios.get('http://localhost:8000')
-    .then(res => {
-        data = res.data;
-        this.setState({
-          details: data
-        });
-      })
-    .catch(err => console.log('Error ${err} occured when GET was called'));
-  }
-
-  render() { 
-    return (
-      <div>
-        <header> Data generated from Django </header>
-        {this.state.details.map((output, id) => 
-          (<div key={id}> 
-            <div>
-              <h2> {output.news} </h2>
-              <h3> {output.detail} </h3>
-            </div>
-          </div>)
-        )} 
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
