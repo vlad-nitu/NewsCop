e318fa02a222d2beea71272a0895583c50793cc7
Rares Toader <R.A.Toader@student.tudelft.nl>
2023-05-07 13:43:17 +0200

    Add unit and integration tests for try url.

--- backend/app/tests/test_urls.py
+++ backend/app/tests/test_urls.py
@@ -3,6 +3,8 @@ from django.urls import resolve, reverse
 from rest_framework import status
 
 from app.views import persist_url_view
+from app.views import try_view
+
 
 class UrlsTest(TestCase):
     @tag("unit")
@@ -16,7 +18,7 @@ class UrlsTest(TestCase):
         self.assertEquals(obtained_url, expected_url)
         self.assertEquals(obtained_view_function, expected_view_function)
 
-    @tag("integration") 
+    @tag("integration")
     def test_persist_url_pattern_post(self):
         expected_persisted_url = 'www.vlad.com'
         client = Client()
@@ -25,9 +27,39 @@ class UrlsTest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.content.decode(), expected_persisted_url)
 
-    @tag("integration") 
+    @tag("integration")
     def test_persist_url_pattern_get_instead_of_post(self):
         client = Client()
         obtained_url = reverse('persist_url', kwargs={'url': 'www.vlad.com'})
         response = client.get(obtained_url)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    @tag("unit")
+    def test_try_path_variable(self):
+        obtained_url = reverse('try', kwargs={'url': 'www.google.com'})
+        expected_url = '/try/www.google.com/'
+
+        obtained_view_function = resolve(obtained_url).func
+        expected_view_function = try_view
+
+        self.assertEquals(obtained_url, expected_url)
+        self.assertEquals(obtained_view_function, expected_view_function)
+
+    @tag("integration")
+    def test_try_pattern_get(self):
+        url = 'www.google.com'
+        expected = "You entered " + url
+        client = Client()
+        obtained_url = reverse('try', kwargs={'url': url})
+        response = client.get(obtained_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.content.decode(), expected)
+
+    @tag("integration")
+    def test_try_pattern_post(self):
+        url = 'www.google.com'
+        client = Client()
+        obtained_url = reverse('try', kwargs={'url': url})
+        response = client.post(obtained_url)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.content.decode(), "Endpoint called with something different than GET")
