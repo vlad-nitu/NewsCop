4fa59e5f51493c296de7d88fccb2184836ffc2f7
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2023-05-08 14:19:37 +0200

    Improved testsuites after MR review

--- backend/app/tests/test_models.py
+++ backend/app/tests/test_models.py
@@ -12,10 +12,36 @@ class FingerprintTest(TestCase):
         self.assertIsInstance(fp.shingle_position, int)
         self.assertEqual(fp.shingle_position, 2)
 class NewsDocumentTest(TestCase):
-    def newsDocument_typeof(self):
+    def test_newsDocument_typeof_single_fp(self):
         fingerprint = Fingerprint(shingle_hash=17, shingle_position=5)
         fp_list = list()
         fp_list.append(fingerprint)
+        url = "www.google.com"
+        published_date = "2022-02-10T10:50:42.389Z"
+        news_doc = NewsDocument(url=url, published_date=published_date, fingerprints=fp_list)
+        self.assertIsInstance(news_doc.url, str)
+        self.assertIsInstance(news_doc.published_date, str)
+        self.assertEqual(news_doc.url, url)
+        self.assertEqual(news_doc.published_date, published_date)
+        self.assertIsInstance(news_doc.fingerprints, list)
+        self.assertEqual(news_doc.fingerprints, fp_list)
+
+    def test_newsDocument_multiple_fingerprints(self):
+        fingerprint1 = Fingerprint(shingle_hash=17, shingle_position=5)
+        fingerprint2 = Fingerprint(shingle_hash=32, shingle_position=2)
+        fp_list = [fingerprint1, fingerprint2]
+        url = "www.google.com"
+        published_date = "2022-02-10T10:50:42.389Z"
+        news_doc = NewsDocument(url=url, published_date=published_date, fingerprints=fp_list)
+        self.assertIsInstance(news_doc.url, str)
+        self.assertIsInstance(news_doc.published_date, str)
+        self.assertEqual(news_doc.url, url)
+        self.assertEqual(news_doc.published_date, published_date)
+        self.assertIsInstance(news_doc.fingerprints, list)
+        self.assertEqual(news_doc.fingerprints, fp_list)
+
+    def test_newsDocument_typeof_no_fps(self):
+        fp_list = []
         url = "www.google.com"
         published_date = "2022-02-10T10:50:42.389Z"
         news_doc = NewsDocument(url=url, published_date=published_date, fingerprints=fp_list)
