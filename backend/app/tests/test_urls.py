e22a4de20bb5d52e5d6f8dc1968a822b8a0a4db7
Rares Toader <R.A.Toader@student.tudelft.nl>
2023-05-08 19:07:22 +0200

    Trigger pipeline run, whitelist an IP that is likely to be gitlab's?

--- backend/app/tests/test_urls.py
+++ backend/app/tests/test_urls.py
@@ -26,7 +26,7 @@ class UrlsTest(TestCase):
         self.assertEquals(obtained_view_function, expected_view_function)
 
     @tag("integration")
-    def test_persist_url_pattern_post(self):  # TODO
+    def test_persist_url_pattern_post(self):
 
         expected_persisted_url = 'www.operatesonmaindb.com'
         client = Client()
