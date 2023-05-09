bfa4e743289a0cb874ac90d3bf394db30e74d469
Micloiu Diana <D.Micloiu@student.tudelft.nl>
2023-05-09 21:15:05 +0200

    Fixing the tests

--- backend/app/plagiarism_checker/fingerprinting.py
+++ backend/app/plagiarism_checker/fingerprinting.py
@@ -1,4 +1,4 @@
 from winnowing import winnow
 
 def compute_fingerprint(article_text):
-    return [[element[0], element[1]] for element in winnow(article_text)]
+    return [{"shingle_hash": element[0], "shingle_position": element[1]} for element in winnow(article_text)]
