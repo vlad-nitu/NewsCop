357d2accd432efd729503e00acb8bf17a1f9216b
Cristian Ciacu <d.c.ciacu@student.tudelft.nl>
2023-05-09 13:23:41 +0200

    fix checkstyle

--- frontend/src/components/map.js
+++ frontend/src/components/map.js
@@ -1,33 +1,9 @@
-import GoogleMapReact from 'google-map-react'
-
 /**
- *
- * @returns HTML Element representing a pin for the map
- */
-const AnyReactComponent = () => <div className='pin1' />
-
-/**
- * Renders a Google Map component with a marker on a specific location.
- *
- * The entire components was built following the tutorial from:
- * https://www.npmjs.com/package/google-map-react
- *
- * TODO: Do conditional rendering based on the device's screen size and do two renderings.
- * The renderings differ mostly on the position of the "location" box.
- * Also, consider the option of NOT rendering the map on small devices.
+ * Map Component.
  *
  * @return {JSX.Element} The Google Map component with a marker on a specific location.
  */
 export default function Map () {
-  // The default props for the Google Map component.
-  const defaultProps = {
-    center: {
-      lat: 51.999631,
-      lng: 4.378186
-    },
-    zoom: 15
-  }
-
   const backgroundStyle = {
     position: 'absolute',
     top: 0,
@@ -36,12 +12,9 @@ export default function Map () {
     height: '100%',
     backgroundImage: 'url(./map.png)',
     backgroundSize: 'cover',
-    backgroundPosition: 'center',
+    backgroundPosition: 'center'
   }
 
-  // TODO: Do conditional rendering based on the device's screen size and do two renderings.
-  // The renderings differ mostly on the position of the "location" box
-  // Also, consider the option of *NOT* rendering the map on small devices.
   return (
     <div style={{ height: '400px', width: '100%' }}>
       <div style={{ height: '100%', width: '100%', position: 'relative' }}>
