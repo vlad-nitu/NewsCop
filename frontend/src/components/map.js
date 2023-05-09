c5be6ad41623388adee4b843e9c6678228ebd2c2
Cristian Ciacu <d.c.ciacu@student.tudelft.nl>
2023-05-09 09:57:29 +0200

    possible fix for map

--- frontend/src/components/map.js
+++ frontend/src/components/map.js
@@ -28,13 +28,24 @@ export default function Map () {
     zoom: 15
   }
 
+  const backgroundStyle = {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundImage: 'url(./map.png)',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  }
+
   // TODO: Do conditional rendering based on the device's screen size and do two renderings.
   // The renderings differ mostly on the position of the "location" box
   // Also, consider the option of *NOT* rendering the map on small devices.
   return (
     <div style={{ height: '400px', width: '100%' }}>
       <div style={{ height: '100%', width: '100%', position: 'relative' }}>
-        <img style={{ height: '100%', width: '100%' }} src="./map.png" alt="map" />
+        <div style={backgroundStyle} />
         <div className='pt-3' style={{ position: 'absolute', left: '100px', top: '50%', transform: 'translateY(-50%)', backgroundColor: '#fff', padding: '10px', boxShadow: '0 0 10px rgba(0,0,0,0.3)' }}>
           <h2 className='title' style={{ color: 'black' }}>Location</h2>
           <p>Van Mourik Broekmanweg 5, 2628 XE Delft</p>
