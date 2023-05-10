6684c25854bdc60689ab19794e9c63388305cd18
Rares Toader <R.A.Toader@student.tudelft.nl>
2023-05-10 19:37:56 +0200

    Add the final text box and the plagiarism information.

--- frontend/src/components/BodyCheckOneText.js
+++ frontend/src/components/BodyCheckOneText.js
@@ -1,24 +1,25 @@
 import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 
+/**
+ * A container that will hold the plagiarism header and description subheader that
+ * can be found on the Figma wireframe.
+ *
+ *
+ * @returns {JSX.Element} that represents the plagiarism text and description
+ */
 export default function BodyCheckOneText () {
-  const description = 'Enter the text of a news article in order to check it for plagiarism'
-  const secondDescription = 'You can do so in the following text box:'
+  const description = 'Plagiarism checker'
+  const secondDescription = 'Our plagiarism checker detects plagiarism in your news article.'
   return (
-    <Container className='my-5' id='ourMission'>
-      <Row>
-        <Col md={6} className='d-flex align-items-center'>
-          <div>
-            <div className='mb-4'>
-              <p className='description-paragraph-first'>{description}</p>
-            </div>
-            <div className='mb-4'>
-              <p className='description-paragraph-second'>{secondDescription}</p>
-            </div>
-          </div>
-        </Col>
-      </Row>
+    <Container className='my-3 d-flex'>
+      <div className='d-flex flex-column justify-content-center mx-auto'>
+        <div className='mb-3 mx-auto'>
+          <h2 className='title' id='plagiarismChecker'>{description}</h2>
+        </div>
+        <div className='mb-4'>
+          <p className='description-paragraph'>{secondDescription}</p>
+        </div>
+      </div>
     </Container>
   )
 }
--- frontend/src/components/TextBox.js
+++ frontend/src/components/TextBox.js
@@ -1,13 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react'
 
+/**
+ * In the service of checking text from an article against plagiarism, we needed a text box to enter information,
+ * and here that is solved.
+ *
+ * @returns {JSX.Element} that is a TextBox where users can enter the news article
+ */
 const TextBox = () => {
   return (
     <div className='d-flex justify-content-center'>
-      <div className='form-group'>
-        <label htmlFor='textBox'>Text Box</label>
-        <textarea type='text' className='form-control custom-textbox' id='textBox' rows='10' />
+      <div className='form-group custom-container'>
+        <label htmlFor='textBox'>Enter the article’s content to check for plagiarism</label>
+        <div className='custom-textarea-container'>
+          <textarea className='form-control custom-textarea' id='textBox' rows='4' />
+        </div>
       </div>
     </div>
   )
 }
+
 export default TextBox
