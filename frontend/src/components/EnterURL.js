0940293d03680fd206fac92419a00038db4a7b26
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2023-05-10 16:44:57 +0200

    Make the Submit button functional + promt the url for testing purposes

--- frontend/src/components/EnterURL.js
+++ frontend/src/components/EnterURL.js
@@ -1,8 +1,34 @@
+import { useState } from 'react'
 import { Container, Form, Button } from 'react-bootstrap'
 
 export default function EnterURL () {
-
   const PreInputArticlePrompt = "Article's URL"
+  const buttonStyle = {
+    width: '25%',
+    height: '6vh',
+    fontWeight: 'bold',
+    fontSize: '1.2rem',
+    backgroundColor: '#2E837E'
+  }
+
+  const [inputValue, setInputValue] = useState('')
+  const [showInputValue, setShowInputValue] = useState(false)
+  const [buttonDisabled, setButtonDisabled] = useState(false)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setShowInputValue(true)
+    setButtonDisabled(true)
+    setTimeout(() => {
+      setShowInputValue(false)
+      setButtonDisabled(false)
+    }, 5000)
+  }
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value)
+    console.log(event.target.value)
+  }
 
   return (
     <Container className='my-3'>
@@ -13,13 +39,31 @@ export default function EnterURL () {
       </div>
       <div style={{ maxWidth: '600px', margin: '0 auto' }}>
         <Form.Group controlId='formUrl'>
-          <Form.Control type='url' placeholder={PreInputArticlePrompt}  className='rounded-pill border-success' />
+          <Form.Control
+            type='url'
+            placeholder={PreInputArticlePrompt}
+            className='rounded-pill border-success'
+            value={inputValue}
+            onChange={handleInputChange}
+          />
         </Form.Group>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Button variant='primary' type='submit' className='mt-4 mx-auto rounded' style={{ width: '25%', height: '6vh', fontWeight: 'bold', fontSize: '1.2rem', backgroundColor: '#2E837E' }}>
+          <Button
+            variant='primary'
+            type='submit'
+            className='mt-4 mx-auto rounded'
+            style={buttonStyle}
+            onClick={handleSubmit}
+            disabled={buttonDisabled}
+          >
             Submit
           </Button>
         </div>
+        {showInputValue && (
+          <div>
+            Input value: "{inputValue}"
+          </div>
+        )}
       </div>
     </Container>
   )
