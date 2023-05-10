3791ef11c35578642257b3c248a033eeed50116a
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2023-05-10 16:19:37 +0200

    Add preprompt

--- frontend/src/components/EnterURL.js
+++ frontend/src/components/EnterURL.js
@@ -1,6 +1,9 @@
 import { Container, Form, Button } from 'react-bootstrap'
 
 export default function EnterURL () {
+
+  const PreInputArticlePrompt = "Article's URL"
+
   return (
     <Container className='my-3'>
       <div className='mt-5'>
@@ -10,7 +13,7 @@ export default function EnterURL () {
       </div>
       <div style={{ maxWidth: '600px', margin: '0 auto' }}>
         <Form.Group controlId='formUrl'>
-          <Form.Control type='url' placeholder='Enter URL' className='rounded-pill border-success' />
+          <Form.Control type='url' placeholder={PreInputArticlePrompt}  className='rounded-pill border-success' />
         </Form.Group>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
           <Button variant='primary' type='submit' className='mt-4 mx-auto rounded' style={{ width: '25%', height: '6vh', fontWeight: 'bold', fontSize: '1.2rem', backgroundColor: '#2E837E' }}>
