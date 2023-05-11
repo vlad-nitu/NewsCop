58a5ed4cb07bfecefc456d38a3b2c35efbe63d31
Micloiu Diana <D.Micloiu@student.tudelft.nl>
2023-05-11 14:17:18 +0300

    Add doc to code

--- backend/app/plagiarism_checker/fingerprinting.py
+++ backend/app/plagiarism_checker/fingerprinting.py
@@ -1,6 +1,14 @@
 from winnowing import winnow
 
 def compute_fingerprint(article_text):
+    '''
+        Function for computing the fingerprint of a given article using winnowing.
+        See the algorithm encapsulated by the winnow function provided in pip package
+        at https://pypi.org/project/winnowing/
+        The function also checks for possible empty text edge case
+        :param article_text: the text of the document to compute the fingerprint for
+        :return: empty list if the text provided is empty or list of fingerprints otherwise
+        '''
     # verify if article text is empty => no text was crawled
     if article_text is None:
         return {}
