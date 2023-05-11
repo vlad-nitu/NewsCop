4b8292b19b79a6eda319a8e2469aa1864d129939
Rares Toader <R.A.Toader@student.tudelft.nl>
2023-05-11 13:32:41 +0200

    Change text in URL page to the other

--- frontend/src/components/CheckURL.js
+++ frontend/src/components/CheckURL.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import EnterURL from './EnterURL'
-import PlagiarismCheckerText from './PlagiarismCheckerText'
 import SecondaryNavbarComponent from './navbarSecondary'
 import Footer from './footer'
 import ForwardToCheckText from './ForwardToCheckText'
+import BodyCheckOneText from "./BodyCheckOneText";
 
 const checkURL = () => {
   const applicationName = 'NewsCop'
-  const title = 'Plagiarism checker'
-  const description = 'Our plagiarims checker detects plagiarism in your news article.'
   const prompt = '... or you may want to check a text paragraph for plagiarism'
 
   return (
@@ -17,7 +15,7 @@ const checkURL = () => {
       <SecondaryNavbarComponent name={applicationName} />
 
       {/* Plagiarism checker Text section */}
-      <PlagiarismCheckerText title={title} description={description} />
+      <BodyCheckOneText/>
 
       {/* Enter article's URL section */}
       <EnterURL />
