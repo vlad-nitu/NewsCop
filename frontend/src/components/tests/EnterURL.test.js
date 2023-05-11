de3322fd3e22617f649ca0a3ce368e1094250d08
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2023-05-11 16:38:58 +0200

    EnterURL test 87.5% coverage

---
+++ frontend/src/components/tests/EnterURL.test.js
@@ -0,0 +1,42 @@
+import { render, screen, fireEvent, getByPlaceholderText } from '@testing-library/react'
+import EnterURL from '../EnterURL'
+
+describe('EnterURL', () => {
+  test('renders the prompt text', () => {
+    const prompt = 'Enter the article\'s URL to check for plagiarism'
+    /*
+     * By wrapping the ForwardToCheckText component with the MemoryRouter,
+     * we provide a routing context that the component can use to render the Link element and trigger navigation events during testing.
+    */
+    render(
+      <EnterURL />
+    )
+
+    // Check if the text element containing the prompt text
+    const promptElement = screen.getByText(prompt)
+    expect(promptElement).toBeInTheDocument()
+
+    // Check if Link component successfully routes the text to '/checkText'
+//    const linkElement = screen.getByRole('link', { name: prompt })
+//    expect(linkElement).toHaveAttribute('href', '/checkText')
+//    expect(linkElement).toHaveClass('description-paragraph')
+//    expect(linkElement).toHaveStyle('color: black')
+//    expect(linkElement).toHaveStyle('fontSize: 150%')
+//    expect(linkElement).toHaveStyle('marginTop: 120px')
+  })
+
+  test('Change input form', () => {
+    const PreInputArticlePrompt = "Article's URL"
+    const { getByPlaceholderText, getByText } = render(<EnterURL />)
+    const input = getByPlaceholderText(PreInputArticlePrompt)
+    const submitButton = getByText('Submit')
+
+    fireEvent.change(input, { target: { value: 'http://example.com/article' } })
+    expect(input.value).toBe('http://example.com/article' )
+
+    fireEvent.click(submitButton)
+    // Even after pressing the Submit button (for 5 seconds), the text remains in the form; after 5 seconds, it gets erased
+    expect(input.value).toBe('http://example.com/article' )
+
+  })
+})
