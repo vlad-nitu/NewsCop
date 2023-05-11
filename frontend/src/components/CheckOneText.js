c24b089d3d4ded1c9ec73faa2c0d1e698ac78032
Rares Toader <R.A.Toader@student.tudelft.nl>
2023-05-11 09:00:44 +0200

    Add documentation

--- frontend/src/components/CheckOneText.js
+++ frontend/src/components/CheckOneText.js
@@ -4,6 +4,12 @@ import BodyCheckOneText from './BodyCheckOneText'
 import TextBox from './TextBox'
 import SubmitButton from './submitButton'
 
+/**
+ * The page for the check text for plagiarism page. It contains all the components that will be present in the page,
+ * and reuses some of the elements that can be found in the main page.
+ *
+ * @returns {JSX.Element} the check text for plagiarism component
+ */
 export default function CheckOneText () {
   const applicationName = 'NewsCop'
 
