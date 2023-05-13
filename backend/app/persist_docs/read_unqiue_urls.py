70bf2b0b0950307815f62a98630e2fa72a50ffc4
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2023-05-13 19:32:21 +0200

    Refactored read_unique_urls.py for testing

--- backend/app/persist_docs/read_unqiue_urls.py
+++ backend/app/persist_docs/read_unqiue_urls.py
@@ -1,11 +1,23 @@
-with open('unique_urls.txt') as f:
-    urls = f.readlines()
+def read_urls_from_file(file_path):
+    with open(file_path) as f:
+        urls = f.readlines()
+    urls = [url.strip() for url in urls]  # Remove leading and trailing spaces
+    urls = [url[2:] if url.startswith('""') else url for url in urls]
+    urls = [url[:-2] if url.endswith('""') else url for url in urls]
+    urls = [url for url in urls if url.startswith("https://")]
+    return urls
 
-urls = [url.strip() for url in urls]  # Remove leading and trailing spaces 
-urls = [url[2:] if url.startswith('""') else url for url in urls]
-urls = [url[:-2] if url.endswith('""') else url for url in urls]
-urls = [url for url in urls if url.startswith("https://")]
-# print('\n'.join(urls)) # prints to stdou
+def write_urls_to_file(urls, file_path):
+    with open(file_path, "w") as out:
+        out.write('\n'.join(urls))
 
-with open("preprocessed_unique_urls.txt", "w") as out:  # write to file
-    out.write('\n'.join(urls))
+def main():
+    input_file = 'unique_urls.txt'
+    output_file = 'preprocessed_unique_urls.txt'
+
+    urls = read_urls_from_file(input_file)
+    write_urls_to_file(urls, output_file)
+
+
+if __name__ == '__main__':
+    main()
