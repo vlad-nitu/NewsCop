dadb527db001f775548806d7d40bfa74976baa19
Micloiu Diana <D.Micloiu@student.tudelft.nl>
2023-05-17 09:36:27 +0200

    Fix checkstyle frontend

--- frontend/src/components/tests/BodyCheckTwoURLs.test.js
+++ frontend/src/components/tests/BodyCheckTwoURLs.test.js
@@ -3,15 +3,15 @@ import { render, screen } from '@testing-library/react'
 import BodyCheckTwoURLs from '../BodyCheckTwoURLs'
 
 describe('BodyCheckTwoURLs', () => {
-    it('renders the component with correct descriptions', () => {
-        render(<BodyCheckTwoURLs />)
+  it('renders the component with correct descriptions', () => {
+    render(<BodyCheckTwoURLs />)
 
-        /* Check if the first description is rendered correctly */
-        const firstDescriptionElement = screen.getByText('News overlap checker')
-        expect(firstDescriptionElement).toBeInTheDocument()
+    /* Check if the first description is rendered correctly */
+    const firstDescriptionElement = screen.getByText('News overlap checker')
+    expect(firstDescriptionElement).toBeInTheDocument()
 
-        /* Check if the second description is rendered correctly */
-        const secondDescriptionElement = screen.getByText('Our similarity checker determines the similarity levels between the content of two news URLs.')
-        expect(secondDescriptionElement).toBeInTheDocument()
-    })
+    /* Check if the second description is rendered correctly */
+    const secondDescriptionElement = screen.getByText('Our similarity checker determines the similarity levels between the content of two news URLs.')
+    expect(secondDescriptionElement).toBeInTheDocument()
+  })
 })
