c9392c120eed4697ab983e0e08606d746aaa9f5a
Micloiu Diana <D.Micloiu@student.tudelft.nl>
2023-05-18 23:59:13 +0200

    Fix front end checkstyle

--- frontend/src/components/tests/EnterTwoURLs.test.js
+++ frontend/src/components/tests/EnterTwoURLs.test.js
@@ -1,99 +1,97 @@
-import {render, screen, fireEvent, act, waitFor} from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import EnterTwoURLs from '../EnterTwoURLs'
-import {MemoryRouter} from "react-router-dom";
-import axios from "axios";
+import { MemoryRouter } from 'react-router-dom'
+import axios from 'axios'
 
 describe('EnterTwoURLs', () => {
-    test('Renders the prompt text', () => {
-        const prompt = 'Enter the article\'s URLs to check for similarity'
-        render(
-            <MemoryRouter>
-                <EnterTwoURLs/>
-            </MemoryRouter>
-        )
+  test('Renders the prompt text', () => {
+    const prompt = 'Enter the article\'s URLs to check for similarity'
+    render(
+      <MemoryRouter>
+        <EnterTwoURLs />
+      </MemoryRouter>
+    )
 
-        // Check if the text element containing the prompt text
-        const promptElement = screen.getByText(prompt)
-        expect(promptElement).toBeInTheDocument()
-    })
+    // Check if the text element containing the prompt text
+    const promptElement = screen.getByText(prompt)
+    expect(promptElement).toBeInTheDocument()
+  })
 
-    test('Input two URLs good', async () => {
-        jest.useFakeTimers() /* Mock the timer */
+  test('Input two URLs good', async () => {
+    jest.useFakeTimers() /* Mock the timer */
 
-        const PreInputArticlePromptOriginal = 'Enter the original URL'
-        const PreInputArticlePromptChanged = 'Enter the changed URL'
-        const {getByPlaceholderText, getByText} = render(<EnterTwoURLs/>)
-        const inputLeft = getByPlaceholderText(PreInputArticlePromptOriginal)
-        const inputRight = getByPlaceholderText(PreInputArticlePromptChanged)
-        const submitButton = getByText('Submit')
+    const PreInputArticlePromptOriginal = 'Enter the original URL'
+    const PreInputArticlePromptChanged = 'Enter the changed URL'
+    const { getByPlaceholderText, getByText } = render(<EnterTwoURLs />)
+    const inputLeft = getByPlaceholderText(PreInputArticlePromptOriginal)
+    const inputRight = getByPlaceholderText(PreInputArticlePromptChanged)
+    const submitButton = getByText('Submit')
 
-        // change the url on the left
-        fireEvent.change(inputLeft, {target: {value: 'https://getbootstrap.com/docs/5.0/forms/layout/'}})
-        expect(inputLeft.value).toBe('https://getbootstrap.com/docs/5.0/forms/layout/')
+    // change the url on the left
+    fireEvent.change(inputLeft, { target: { value: 'https://getbootstrap.com/docs/5.0/forms/layout/' } })
+    expect(inputLeft.value).toBe('https://getbootstrap.com/docs/5.0/forms/layout/')
 
-        // change the url on the right
-        fireEvent.change(inputRight, {target: {value: 'https://getbootstrap.com/docs/5.0/forms/validation/'}})
-        expect(inputRight.value).toBe('https://getbootstrap.com/docs/5.0/forms/validation/')
+    // change the url on the right
+    fireEvent.change(inputRight, { target: { value: 'https://getbootstrap.com/docs/5.0/forms/validation/' } })
+    expect(inputRight.value).toBe('https://getbootstrap.com/docs/5.0/forms/validation/')
 
-        jest.mock('axios')
-        const expectedData = {similarity: 0.13}
-        axios.post = jest.fn().mockResolvedValueOnce({data: expectedData})
+    jest.mock('axios')
+    const expectedData = { similarity: 0.13 }
+    axios.post = jest.fn().mockResolvedValueOnce({ data: expectedData })
 
+    fireEvent.click(submitButton)
 
-        fireEvent.click(submitButton)
+    // Even after pressing the Submit button (for 5 seconds), the text remains in the form; after 5 seconds, it gets erased
+    expect(inputLeft.value).toBe('https://getbootstrap.com/docs/5.0/forms/layout/')
+    expect(inputRight.value).toBe('https://getbootstrap.com/docs/5.0/forms/validation/')
+    expect(submitButton).toBeDisabled()
+    expect(inputRight).toBeDisabled()
+    expect(inputLeft).toBeDisabled()
 
-        // Even after pressing the Submit button (for 5 seconds), the text remains in the form; after 5 seconds, it gets erased
-        expect(inputLeft.value).toBe('https://getbootstrap.com/docs/5.0/forms/layout/')
-        expect(inputRight.value).toBe('https://getbootstrap.com/docs/5.0/forms/validation/')
-        expect(submitButton).toBeDisabled()
-        expect(inputRight).toBeDisabled()
-        expect(inputLeft).toBeDisabled()
+    // act(() => {
+    //     jest.advanceTimersByTime(5000) /* Advance timer by 5 seconds */
+    // })
+    //
+    // await waitFor(() => {
+    //     expect(submitButton).toBeEnabled()
+    // }) /* Button should be re-enabled after 5 seconds */
+    //
+    // expect(inputLeft).toBeEnabled()
+    // expect(inputRight).toBeEnabled()
+    // expect(inputLeft.value).toBe('https://getbootstrap.com/docs/5.0/forms/layout/')
+    // expect(inputRight.value).toBe('https://getbootstrap.com/docs/5.0/forms/validation/')
+  })
 
-        // act(() => {
-        //     jest.advanceTimersByTime(5000) /* Advance timer by 5 seconds */
-        // })
-        //
-        // await waitFor(() => {
-        //     expect(submitButton).toBeEnabled()
-        // }) /* Button should be re-enabled after 5 seconds */
-        //
-        // expect(inputLeft).toBeEnabled()
-        // expect(inputRight).toBeEnabled()
-        // expect(inputLeft.value).toBe('https://getbootstrap.com/docs/5.0/forms/layout/')
-        // expect(inputRight.value).toBe('https://getbootstrap.com/docs/5.0/forms/validation/')
+  test('Input two empty URLs', () => {
+    render(<EnterTwoURLs />)
+    const submitButton = screen.getByText('Submit')
 
-    })
+    expect(submitButton).toBeDisabled()
+  })
 
-    test('Input two empty URLs', () => {
-        render(<EnterTwoURLs />);
-        const submitButton = screen.getByText('Submit');
+  test('Input two URLs only the left', () => {
+    const PreInputArticlePromptOriginal = 'Enter the original URL'
+    const { getByPlaceholderText, getByText } = render(<EnterTwoURLs />)
+    const inputLeft = getByPlaceholderText(PreInputArticlePromptOriginal)
+    const submitButton = getByText('Submit')
 
-        expect(submitButton).toBeDisabled();
-    });
+    // change the url on the left
+    fireEvent.change(inputLeft, { target: { value: 'http://example.com/article' } })
+    expect(inputLeft.value).toBe('http://example.com/article')
 
-    test('Input two URLs only the left', () => {
-        const PreInputArticlePromptOriginal = 'Enter the original URL'
-        const {getByPlaceholderText, getByText} = render(<EnterTwoURLs/>)
-        const inputLeft = getByPlaceholderText(PreInputArticlePromptOriginal)
-        const submitButton = getByText('Submit')
+    expect(submitButton).toBeDisabled()
+  })
 
-        // change the url on the left
-        fireEvent.change(inputLeft, {target: {value: 'http://example.com/article'}})
-        expect(inputLeft.value).toBe('http://example.com/article')
+  test('Input two URLs only the right', () => {
+    const PreInputArticlePromptChanged = 'Enter the changed URL'
+    const { getByPlaceholderText, getByText } = render(<EnterTwoURLs />)
+    const inputRight = getByPlaceholderText(PreInputArticlePromptChanged)
+    const submitButton = getByText('Submit')
 
-        expect(submitButton).toBeDisabled()
-    })
+    // change the url on the right
+    fireEvent.change(inputRight, { target: { value: 'http://example.com/article' } })
+    expect(inputRight.value).toBe('http://example.com/article')
 
-    test('Input two URLs only the right', () => {
-        const PreInputArticlePromptChanged = 'Enter the changed URL'
-        const {getByPlaceholderText, getByText} = render(<EnterTwoURLs/>)
-        const inputRight = getByPlaceholderText(PreInputArticlePromptChanged)
-        const submitButton = getByText('Submit')
-
-        // change the url on the right
-        fireEvent.change(inputRight, {target: {value: 'http://example.com/article'}})
-        expect(inputRight.value).toBe('http://example.com/article')
-
-        expect(submitButton).toBeDisabled()
-    })
+    expect(submitButton).toBeDisabled()
+  })
 })
