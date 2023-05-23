50a588a24910cda716aee41aaca2b7fe16b66957
Rares Toader <R.A.Toader@student.tudelft.nl>
2023-05-23 20:35:42 +0200

    batch processing persist url

--- backend/app/models.py
+++ backend/app/models.py
@@ -25,19 +25,29 @@ class NewsDocument(Document):
     published_date = DateTimeField()
     fingerprints = ListField(IntField())
 
-    def save(self, *args, **kwargs):
-        db.rares_news_collection.insert_one({
+    def save(self):
+        # Implemented in a batch processing fashion
+        visited_fps = set()  # the fingerprints that the current document has
+        doc = {
             '_id': self.url,
             'published_date': self.published_date,
             'fingerprints': self.fingerprints
-        })
-        for i in self.fingerprints:
-            hash_exists = db.rares_hashes.find_one({'_id': i}) is not None
-            if hash_exists:
-                db.rares_hashes.update_one({"_id": i}, {"$addToSet": {"urls": self.url}})
-            else:
-                hash_set = [self.url]
-                db.rares_hashes.insert_one({
-                    '_id': i,
-                    'urls': hash_set
-                })
+        }
+        db.rares_news_collection.insert_one(doc)
+
+        for fp in self.fingerprints:
+            if fp not in visited_fps:
+                visited_fps.add(fp)
+        existing_fps = set(
+            [x['_id'] for x in db.rares_hashes.find({}, {"_id": 1})])  # the already existing fps in the collection
+        need_to_update_fps = visited_fps & existing_fps  # the already existing fp_s
+        need_to_insert_fps = visited_fps - need_to_update_fps  # the fp_s that need to be inserted
+        # Update matching documents in rares_hashes collection
+
+        filter_condition = {"_id": {"$in": list(need_to_update_fps)}}
+        update_query = {"$addToSet": {"urls": self.url}}
+        db.rares_hashes.update_many(filter_condition, update_query)
+        to_insert = []
+        for i in need_to_insert_fps:
+            to_insert.append({"_id": i, "urls": [self.url]})
+        db.rares_hashes.insert_many(to_insert)
