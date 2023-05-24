094254b8f707cc61adb8e0ca4c7002ecc95e4534
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2023-05-24 17:48:21 +0200

    Interpolate from Green to Red

--- frontend/src/components/ProgressBarCustom.js
+++ frontend/src/components/ProgressBarCustom.js
@@ -1,48 +1,55 @@
 import React from 'react';
-import ProgressBar from 'react-customizable-progressbar'
+import ProgressBar from 'react-customizable-progressbar';
 
-export default function ProgressBarCustom(similarity) {
+const CustomProgressBar = ({ similarity }) => {
 
- // Function to obtain the color needed for interpolation
- const colorInterpolation = (similarityScore) =>  {
+  const interpolateColor = (similarity) => {
 
-    if (similarityScore >= 0  && similarityScore <= 20)
-      return "#28aa4a" // Green
-    else if (similarityScore > 20 && similarityScore <= 40)
-      return "#8abb3c" // Yellow
-    else if (similarityScore > 40 && similarityScore <= 60)
-      return "#eeea24" // Yellow
-    else if (similarityScore > 60 && similarityScore <= 80)
-      return "#ed9e28"
-    else
-      return "#e73626"
- }
+  const percentage = similarity / 100
+    console.log(percentage)
 
+  // Define the start and end colors (red and green)
+  const [startR, startG, startB] = [0, 255, 0];   // Red
+  const [endR, endG, endB] = [255, 0, 0];     // Green
 
-  // Function to interpolate the similarity value
-  const interpolateSimilarity = (similarity) => {
-    const similarityScore = similarity.similarity
-    const color = colorInterpolation(similarityScore)
-    console.log(similarity)
-    console.log(similarityScore)
-    return color;
-  };
+  // Calculate the interpolated RGB values
+  const interpolatedR = Math.round(startR + (endR - startR) * percentage);
+  const interpolatedG = Math.round(startG + (endG - startG) * percentage);
+  const interpolatedB = Math.round(startB + (endB - startB) * percentage);
+
+  // Convert the interpolated RGB values to a hexadecimal string
+  const interpolatedHex = `#${componentToHex(interpolatedR)}${componentToHex(interpolatedG)}${componentToHex(interpolatedB)}`;
+
+    console.log(interpolatedHex)
+    console.log(interpolatedR + ' ' + interpolatedG + ' ' + interpolatedB)
+
+  // Return the interpolated color as a hexadecimal string
+  return interpolatedHex;
+}
+
+// Helper function to convert a decimal color component to a two-digit hexadecimal string
+function componentToHex(c) {
+  const hex = c.toString(16);
+  return hex.length === 1 ? `0${hex}` : hex;
+};
 
   return (
     <div className='mb-3 mt-5' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <ProgressBar
         radius={100}
-        progress={similarity.similarity}
+        progress={similarity}
         counterClockwise
         strokeWidth={18}
-        strokeColor={interpolateSimilarity(similarity)}
+        strokeColor={interpolateColor(similarity)}
         trackStrokeWidth={18}
         trackStrokeColor="#f4f4f4"
         pointerRadius={10}
         pointerStrokeWidth={1}
         pointerStrokeColor="#ffffff"
         pointerFillColor="#000000"
-    />
+      />
     </div>
   );
 };
+
+export default CustomProgressBar;
