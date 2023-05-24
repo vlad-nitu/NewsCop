7ee47ee9a82f0fd7225916c8c17bbab165210955
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2023-05-24 17:08:38 +0200

    5 colors from green to red, 20% scale
    Not interpolated yet

--- frontend/src/components/ProgressBarCustom.js
+++ frontend/src/components/ProgressBarCustom.js
@@ -2,22 +2,40 @@ import React from 'react';
 import ProgressBar from 'react-customizable-progressbar'
 
 export default function ProgressBarCustom(similarity) {
+
+ // Function to obtain the color needed for interpolation
+ const colorInterpolation = (similarityScore) =>  {
+
+    if (similarityScore >= 0  && similarityScore <= 20)
+      return "#28aa4a" // Green
+    else if (similarityScore > 20 && similarityScore <= 40)
+      return "#8abb3c" // Yellow
+    else if (similarityScore > 40 && similarityScore <= 60)
+      return "#eeea24" // Yellow
+    else if (similarityScore > 60 && similarityScore <= 80)
+      return "#ed9e28"
+    else
+      return "#e73626"
+ }
+
+
   // Function to interpolate the similarity value
   const interpolateSimilarity = (similarity) => {
-    const percentage = similarity.similarity
+    const similarityScore = similarity.similarity
+    const color = colorInterpolation(similarityScore)
     console.log(similarity)
-    console.log(percentage)
-    return percentage;
+    console.log(similarityScore)
+    return color;
   };
 
   return (
     <div className='mb-3' style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <ProgressBar
         radius={100}
-        progress={interpolateSimilarity(similarity)}
+        progress={similarity.similarity}
         counterClockwise
         strokeWidth={18}
-        strokeColor="#a0d468"
+        strokeColor={interpolateSimilarity(similarity)}
         trackStrokeWidth={18}
         trackStrokeColor="#f4f4f4"
         pointerRadius={10}
