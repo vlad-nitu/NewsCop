cafee17b67e98e9098c84ee8bc8694a2be123fb3
Matei Mirica <M.M.Mirica@student.tudelft.nl>
2023-05-26 13:10:10 +0200

    fix documentation

--- frontend/src/components/CheckUrlDecision.js
+++ frontend/src/components/CheckUrlDecision.js
@@ -3,6 +3,7 @@ import ListURLs from './ListURLs'
 /**
  * The decision that is shown after entering the url.
  * @param items the list of articles which will be displayed
+ * @param similarities the list of similarities with the given articles used to display the progress bar
  * @param publishingDate the publishing date of the article
  * @param decision the decision, i.e. whether the article was plagiarised
  * @returns {JSX.Element} the element that contains the decision
--- frontend/src/components/ListURLs.js
+++ frontend/src/components/ListURLs.js
@@ -3,6 +3,7 @@ import ProgressLineCustom from './ProgressLineCustom'
 /**
  * Displays a list of articles together with their similarities
  * @param items the list of articles which will be displayed
+ * @param similarities the list of similarities with the given articles used to display the progress bar
  * @returns {JSX.Element} the element that contains the list
  */
 
