fe8af9cf5e1a983c27ba76a81ac63834ebd1917d
Matei Mirica <M.M.Mirica@student.tudelft.nl>
2023-05-26 14:55:00 +0200

    remove commented prints

--- backend/app/views.py
+++ backend/app/views.py
@@ -96,7 +96,6 @@ def persist_url_view(request):
 
         # Check whether the current URL is present in the database
         url_exists = db.news_collection.find_one({'_id': url}) is not None
-        # print(f'Url does exist: {url_exists}')
 
         # If current URL is not part of the database, persist it
         if not url_exists:
@@ -114,13 +113,9 @@ def persist_url_view(request):
             if len(only_shingle_values) == 0:
                 return HttpResponseBadRequest("The article provided has no text.")
 
-            # print(only_shingle_values)
             newsdoc = NewsDocument(url=url, published_date=article_date, fingerprints=only_shingle_values)
             newsdoc.save()
 
-            # print(len(only_shingle_values))
-
-        # print("persist_url_view: " + url)
         return HttpResponse(url, status=200)
 
     else:
