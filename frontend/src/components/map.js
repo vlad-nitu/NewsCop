16d697104131966a420237e4b7e0e1d4d6959df1
Cristian Ciacu <d.c.ciacu@student.tudelft.nl>
2023-05-31 15:57:31 +0200

    incorporate new map library + marker pointing to echo

--- frontend/src/components/map.js
+++ frontend/src/components/map.js
@@ -1,29 +1,31 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
 /**
  * Map Component.
  *
- * @return {JSX.Element} The Google Map component with a marker on a specific location.
+ * @return {JSX.Element} The Map component with a marker on a specific location.
  */
 export default function Map () {
-  const backgroundStyle = {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-    backgroundImage: 'url(./map.png)',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center'
-  }
+
+  const position = [51.999512, 4.377728]
 
   return (
-    <div style={{ height: '400px', width: '100%' }}>
-      <div style={{ height: '100%', width: '100%', position: 'relative' }}>
-        <div style={backgroundStyle} data-testid='backgroundStyle' />
-        <div className='pt-3' style={{ position: 'absolute', left: '100px', top: '50%', transform: 'translateY(-50%)', backgroundColor: '#fff', padding: '10px', boxShadow: '0 0 10px rgba(0,0,0,0.3)' }}>
-          <h2 className='title' style={{ color: 'black' }}>Location</h2>
-          <p>Van Mourik Broekmanweg 5, 2628 XE Delft</p>
-        </div>
-      </div>
-    </div>
+    <MapContainer
+      className="markercluster-map"
+      center={position}
+      zoom={17}
+      maxZoom={18}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   )
 }
