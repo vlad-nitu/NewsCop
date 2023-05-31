fdbaf4d7e3dd44781aa1e27ade59a50f42283cd2
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2023-05-31 12:06:43 +0200

    Fully tested TextBox

--- frontend/src/components/TextBox.js
+++ frontend/src/components/TextBox.js
@@ -4,6 +4,45 @@ import { RichTextarea } from 'rich-textarea'
 import Highlighter from 'react-highlight-words'
 
 /**
+ * Generates an array of word chunks representing the highlighted words in a given text.
+ *
+ * @param {Object} options - The options for highlighting.
+ * @param {boolean} options.autoEscape - Whether to automatically escape special characters in search words.
+ * @param {boolean} options.caseSensitive - Whether the search should be case-sensitive.
+ * @param {boolean} options.sanitize - Whether to sanitize the search words for highlighting.
+ * @param {string[]} options.searchWords - The array of search words to highlight.
+ * @param {string} options.textToHighlight - The text to highlight.
+ * @param {number} options.similarity - The similarity score. If 0, an empty array is returned.
+ * @returns {Array} - An array of word chunks representing the highlighted words.
+ */
+export const highlightWordsOnly = ({ autoEscape, caseSensitive, sanitize, searchWords, textToHighlight, similarity }) => {
+  if (similarity === 0) {
+    return [] // Return an empty array if similarity is 0
+  }
+
+  const words = textToHighlight.trim().split(/\s+/) // Split the text into an array of words
+
+  const chunks = []
+  let currentIndex = 0
+
+  words.forEach((word) => {
+    const matchedWord = searchWords.find((searchWord) =>
+      caseSensitive ? word === searchWord : word.toLowerCase() === searchWord.toLowerCase()
+    )
+
+    if (matchedWord) {
+      const startIndex = textToHighlight.indexOf(word, currentIndex)
+      const endIndex = startIndex + word.length
+      chunks.push({ start: startIndex, end: endIndex })
+    }
+
+    currentIndex += word.length + 1 // Add 1 for the space between words
+  })
+
+  return chunks
+}
+
+/**
  * In the service of checking text from an article against plagiarism, we needed a text box to enter information,
  * and here that is solved.
  *
@@ -14,33 +53,6 @@ const TextBox = ({
 }) => {
   const handleTextAreaChange = (event) => {
     if (setTextAreaValue != null) { setTextAreaValue(event.target.value) }
-  }
-
-  const highlightWordsOnly = ({ autoEscape, caseSensitive, sanitize, searchWords, textToHighlight, similarity }) => {
-    if (similarity === 0) {
-      return [] // Return an empty array if similarity is 0
-    }
-
-    const words = textToHighlight.trim().split(/\s+/) // Split the text into an array of words
-
-    const chunks = []
-    let currentIndex = 0
-
-    words.forEach((word) => {
-      const matchedWord = searchWords.find((searchWord) =>
-        caseSensitive ? word === searchWord : word.toLowerCase() === searchWord.toLowerCase()
-      )
-
-      if (matchedWord) {
-        const startIndex = textToHighlight.indexOf(word, currentIndex)
-        const endIndex = startIndex + word.length
-        chunks.push({ start: startIndex, end: endIndex })
-      }
-
-      currentIndex += word.length + 1 // Add 1 for the space between words
-    })
-
-    return chunks
   }
 
   return (
--- frontend/src/components/tests/TextBox.test.js
+++ frontend/src/components/tests/TextBox.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import TextBox from '../TextBox'
+import TextBox, { highlightWordsOnly } from '../TextBox'
 import ResizeObserver from 'resize-observer-polyfill'
 
 if (!window.ResizeObserver) {
@@ -13,5 +13,49 @@ describe('TextBox', () => {
     /* Check if the text area is rendered correctly */
     const textareaElement = screen.getByPlaceholderText('Enter your article here')
     expect(textareaElement).toBeInTheDocument()
+  })
+
+  describe('highlightWordsOnly function', () => {
+    test('returns an empty array if similarity is 0', () => {
+      const result = highlightWordsOnly({
+        autoEscape: true,
+        caseSensitive: false,
+        sanitize: true,
+        searchWords: ['word1', 'word2'],
+        textToHighlight: 'some text',
+        similarity: 0
+      })
+
+      expect(result).toEqual([])
+    })
+
+    test('finds and returns chunks for matched words', () => {
+      const result = highlightWordsOnly({
+        autoEscape: true,
+        caseSensitive: false,
+        sanitize: true,
+        searchWords: ['fox', 'dog'],
+        textToHighlight: 'The quick brown fox jumps over the lazy dog',
+        similarity: 1
+      })
+
+      expect(result).toEqual([
+        { start: 16, end: 19 }, // chunk for 'fox'
+        { start: 40, end: 43 } // chunk for 'dog'
+      ])
+    })
+
+    test('handles case sensitivity properly', () => {
+      const result = highlightWordsOnly({
+        autoEscape: true,
+        caseSensitive: true,
+        sanitize: true,
+        searchWords: ['Fox'],
+        textToHighlight: 'The quick brown fox jumps over the lazy dog',
+        similarity: 1
+      })
+
+      expect(result).toEqual([])
+    })
   })
 })
