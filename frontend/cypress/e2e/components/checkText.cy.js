f71f2c8e1a32abc5077fc29dffcf87153981640e
Rares Toader <R.A.Toader@student.tudelft.nl>
2023-06-03 18:56:21 +0200

    add last tests for check text

--- frontend/cypress/e2e/components/checkText.cy.js
+++ frontend/cypress/e2e/components/checkText.cy.js
@@ -1,5 +1,3 @@
-import { Link } from 'react-router-dom'
-
 describe('tests for several interactions within check text', () => {
 
   const rootUrl = 'http://localhost:3000'
@@ -10,82 +8,83 @@ describe('tests for several interactions within check text', () => {
     cy.visit(`${rootUrl}/checkText`)
   })
 
-  // it('Components rendered correctly and small interactions', () => {
-  //
-  //   /* The navbar is rendered and has the right name */
-  //   cy.get('[data-testid="navbar-component"]')
-  //     .should('exist')
-  //     .should('contain', 'NewsCop')
-  //
-  //   /* Verify that the BodyCheckGeneric component is rendered with the correct description */
-  //   cy.get('[data-testid="body-check-generic"]')
-  //     .should('exist')
-  //     .should('contain', 'News overlap checker') // First description
-  //     .should('contain', 'Our tool detects overlap in your news article.') // Second Description
-  //
-  //   /* Button is rendered correctly but is disabled */
-  //   cy.get('[data-testid="submit_button"]')
-  //     .should('exist')
-  //     .and('be.disabled')
-  //
-  //   /* Verify that the text area + the text above it are rendered correctly */
-  //   cy.get('[data-testid="textAreaCheckOneText"]')
-  //     .should('exist')
-  //     .should('have.attr', 'placeholder', 'Enter your article here') // Placeholder value
-  //     .should('have.value', '') // No text in the box currently
-  //     .type('Example Test') // Write something in the box
-  //     .should('have.value', 'Example Test') // See the text in the box changing
-  //
-  //   /* The text above the box */
-  //   cy.contains('h2', 'Enter the article’s content to check for overlap').should('exist');
-  //
-  //   /* The error-prompt does not exist */
-  //   cy.get('[data-testid="error-prompt"]')
-  //     .should('not.exist')
-  //
-  //   /* Check that the submit button is rendered and is not disabled*/
-  //   cy.get('[data-testid="submit_button"]')
-  //     .should('exist')
-  //     // The button should not be disabled because there is text already in the box
-  //     .and('not.be.disabled')
-  //     .should('have.text', 'Submit')
-  //     .click()
-  //
-  //   /* The error-prompt exists and is visible */
-  //   cy.get('[data-testid="error-prompt"]')
-  //     .should('exist')
-  //     .and('be.visible')
-  //     .should('have.text', 'You entered an invalid text!')
-  //
-  //   /* The button is temporarily disabled */
-  //   cy.get('[data-testid="submit_button"]')
-  //     .should('exist')
-  //     .and('be.disabled')
-  //     .wait(10000)
-  //     .should('be.enabled')
-  //
-  //   /* The error-prompt does not exist after timeout */
-  //   cy.get('[data-testid="error-prompt"]')
-  //     .should('not.exist')
-  //
-  //   /* The footer is rendered correctly */
-  //   cy.get('#footer')
-  //     .should('exist')
-  //
-  //   /* The loading circle should not exist */
-  //   cy.get('[data-testid="loading-circle"]')
-  //     .should('not.exist')
-  //
-  //   /* The forward to page should exist */
-  //   cy.get('[data-testid="forward-to-page"]')
-  //     .should('exist')
-  //     .should('have.text', '... or you may want to check a news article via an URL for similarity')
-  //
-  //   /* The check decision should not be visible */
-  //   cy.get('[data-testid="check-decision"]')
-  //     .should('exist')
-  //     .should('not.be.visible')
-  // })
+  it('Components rendered correctly and small interactions', () => {
+
+    /* The navbar is rendered and has the right name */
+    cy.get('[data-testid="navbar-component"]')
+      .should('exist')
+      .should('contain', 'NewsCop')
+
+    /* Verify that the BodyCheckGeneric component is rendered with the correct description */
+    cy.get('[data-testid="body-check-generic"]')
+      .should('exist')
+      .should('contain', 'News overlap checker') // First description
+      .should('contain', 'Our tool detects overlap in your news article.') // Second Description
+
+    /* Button is rendered correctly but is disabled */
+    cy.get('[data-testid="submit_button"]')
+      .should('exist')
+      .and('be.disabled')
+
+    /* Verify that the text area + the text above it are rendered correctly */
+    cy.get('[data-testid="textAreaCheckOneText"]')
+      .should('exist')
+      .should('have.attr', 'placeholder', 'Enter your article here') // Placeholder value
+      .should('have.value', '') // No text in the box currently
+      .type('Example Test') // Write something in the box
+      .should('have.value', 'Example Test') // See the text in the box changing
+
+    /* The text above the box */
+    cy.contains('h2', 'Enter the article’s content to check for overlap').should('exist');
+
+    /* The error-prompt does not exist */
+    cy.get('[data-testid="error-prompt"]')
+      .should('not.exist')
+
+    /* Check that the submit button is rendered and is not disabled*/
+    cy.get('[data-testid="submit_button"]')
+      .should('exist')
+      // The button should not be disabled because there is text already in the box
+      .and('not.be.disabled')
+      .should('have.text', 'Submit')
+      .click()
+
+    /* The error-prompt exists and is visible */
+    cy.get('[data-testid="error-prompt"]')
+      .should('exist')
+      .and('be.visible')
+      .should('have.text', 'You entered an invalid text!')
+
+    /* The button is temporarily disabled */
+    cy.get('[data-testid="submit_button"]')
+      .should('exist')
+      .and('be.disabled')
+      .wait(10000)
+      .should('be.enabled')
+
+    /* The error-prompt does not exist after timeout */
+    cy.get('[data-testid="error-prompt"]')
+      .should('not.exist')
+
+    /* The footer is rendered correctly */
+    cy.get('#footer')
+      .should('exist')
+
+    /* The loading circle should not exist */
+    cy.get('[data-testid="loading-circle"]')
+      .should('not.exist')
+
+    /* The forward to page should exist */
+    cy.get('[data-testid="forward-to-page"]')
+      .should('exist')
+      .should('have.text', '... or you may want to check a news article via an URL for similarity')
+
+    /* The check decision should not be visible */
+    cy.get('[data-testid="check-decision"]')
+      .should('exist')
+      .should('not.be.visible')
+  })
+
   it('Redirect to checkURL page', () => {
     /* The forward to page should exist and small interaction */
     cy.get('[data-testid="forward-to-link"]')
@@ -212,7 +211,8 @@ describe('tests for several interactions within check text', () => {
     cy.url()
       .should('be.equal', `${rootUrl}/compareTexts`) // We should now be in the compare texts page
   })
-  it('Redirect to compare texts via Footer', () => {
+
+  it('Redirect to compare URLs via Footer', () => {
     cy.get('[data-testid="URLSim"]') // Retrieve the compare URLs link in the footer
       .should('exist')
       .should('contain', 'Similarity checker for two URLs')
@@ -220,6 +220,32 @@ describe('tests for several interactions within check text', () => {
       .wait(100) // Wait a moment until everything is rendered
     cy.url()
       .should('be.equal', `${rootUrl}/compareURLs`) // We should now be in the compare URLs page
+  })
+  it('Redirection to main page through About Us', () => {
+    /** Retrieve our mission component from the navbar **/
+    cy.get('[data-testid="navbar-component"]')
+      .should('exist')
+      .get('a[href*="/#ourMission"]')
+      .click()
+    cy.url()
+      .should(
+        'be.equal',
+        `${rootUrl}/#ourMission`)
+  })
+
+  it('Redirection to main page through Contact Us', () => {
+    cy.get('[data-testid="navbar-component"]')
+      .should('exist')
+      .get('a[href="#footer"]')
+      .contains('Contact')
+      .click()
+      .wait(500)
+    /* The viewport should now be visible */
+    cy.url()
+      .should(
+        'be.equal',
+        `${rootUrl}/checkText#footer`)
+    cy.isInViewport('#footer')
   })
 
 })
