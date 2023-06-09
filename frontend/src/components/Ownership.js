7615ff667088bb8bded40d69a1fe213ef6592667
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2023-06-09 15:20:50 +0200

    Tesed Ownership functionality on frontend

--- frontend/src/components/Ownership.js
+++ frontend/src/components/Ownership.js
@@ -4,7 +4,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 /**
  * The Ownership that is shown after entering two URLs, if both dates were able to be retrieved 
  * and there is a high overlap between the two aricles.
- * @param prompt the prompt of the error
+ * It also displays an arrow in the Source's colors to indicate which URL was the first publisher
+ * * @param prompt the prompt of the error
  * @returns {JSX.Element} the element that contains the error
  */
 
@@ -15,11 +16,11 @@ export default function Ownership({ result }) {
         result.includes("right") ? (
           <>
             <h2 className="description-overlap d-inline-block" data-testid="ownership-prompt"> {result}</h2>
-            <FontAwesomeIcon icon={faArrowTurnUp} style={{ color: "#2e837e", }} className="d-inline-block float-right turn-up-arrow-icon ml-2" />
+            <FontAwesomeIcon icon={faArrowTurnUp} style={{ color: "#2e837e", }} className="d-inline-block float-right turn-up-arrow-icon ml-2" data-testid="turn-up-icon"/>
           </>
         ) : (
           <>
-            <FontAwesomeIcon icon={faArrowTurnUp} flip="horizontal" style={{ color: "#2e837e", }} className="d-inline-block float-right turn-up-arrow-icon ml-2" />
+            <FontAwesomeIcon icon={faArrowTurnUp} flip="horizontal" style={{ color: "#2e837e", }} className="d-inline-block float-right turn-up-arrow-icon ml-2" data-testid="turn-up-icon"/>
             <h2 className="description-overlap d-inline-block" data-testid="ownership-prompt"> {result}</h2>
           </>
         )
