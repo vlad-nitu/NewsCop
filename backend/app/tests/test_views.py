4be903617def0fe5caf6b9fc434daca7a730c5d2
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2023-06-13 12:10:49 +0200

    Fixed sets clean up properly

--- backend/app/tests/test_views.py
+++ backend/app/tests/test_views.py
@@ -379,6 +379,7 @@ class TestUrlSimilarity(TestCase):
         self.cursor.execute(f'DELETE FROM {schema}.url_fingerprints')
         self.cursor.execute(f'DELETE FROM {schema}.urls')
         self.cursor.execute(f'DELETE FROM {schema}.fingerprints')
+        conn.commit()  # commit the changes
         existing_fps.clear()
         self.cursor.close()
 
@@ -388,6 +389,7 @@ class TestUrlSimilarity(TestCase):
         self.cursor.execute(f'DELETE FROM {schema}.url_fingerprints')
         self.cursor.execute(f'DELETE FROM {schema}.urls')
         self.cursor.execute(f'DELETE FROM {schema}.fingerprints')
+        conn.commit()  # commit the changes
         existing_fps.clear()
         self.cursor.close()
 
