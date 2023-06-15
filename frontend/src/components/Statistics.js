0102b16ecf8a8d8d94401bc3e3cbf7143660a69b
Cristian Ciacu <d.c.ciacu@student.tudelft.nl>
2023-06-15 09:22:56 +0200

    add bar statistics, pipeline will fail haha

--- frontend/src/components/Statistics.js
+++ frontend/src/components/Statistics.js
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import { Card } from 'react-bootstrap'
+import { useState, useEffect } from 'react'
 
 /**
  *
@@ -22,6 +23,24 @@ export default function Statistics ({ titles, descriptions, images }) {
         descriptions.length !== images.length) {
     throw Error('Different lengths of the arrays.')
   }
+
+  const [width, setWidth] = useState(window.innerWidth)
+
+  /**
+   * Update the width on resizing
+   */
+  const updateDimensions = () => {
+    setWidth(window.innerWidth)
+  }
+
+  /**
+   * When the size of the windows are changed call the updateDimensions method
+   */
+  useEffect(() => {
+    window.addEventListener('resize', updateDimensions)
+    return () => window.removeEventListener('resize', updateDimensions)
+  }, [])
+
   // Renders a JSX element with information about statistics.
   return (
     <div id='statistics'>
@@ -31,7 +50,7 @@ export default function Statistics ({ titles, descriptions, images }) {
           {titles.map((title, index) => (
             <Col key={index} xs={12} sm={12} md={4} lg={4} className='mb-3 align-items-stretch'>
               <Card className='custom-statistics-card text-center mt-3 h-100'>
-                <Card.Img alt={`Service ${index + 1}`} src={images[index]} className='card-img mx-auto'/>
+                <Card.Img alt={`Service ${index + 1}`} src={images[index]} className='card-img mx-auto' />
                 <Card.Body className='d-flex flex-column px-0'>
                   <Card.Title className='font-weight-bold'>{title}</Card.Title>
                   <Card.Text className='mb-4'>
@@ -42,7 +61,75 @@ export default function Statistics ({ titles, descriptions, images }) {
             </Col>
           ))}
         </Row>
-        <h2 className='statistics-title'>Number of articles for each overlap threshold</h2>
+        <div className='pt-5'>
+          {true
+            ? (
+              <div id='bar-statistics' className='d-flex flex-row'>
+                <div id='left-side-text' className='mt-auto flex-shrink-1 pe-5' style={{ width: '350px' }}>
+                  <p className='fs-4 fw-bolder m-0'>Number of articles for each overlap threshold</p>
+                  <p className='fs-5 text-muted mt-0'>from our database</p>
+                </div>
+                <div id='bar1' className='pe-2 flex-grow-1'>
+                  <div style={{ height: '300px' }} className='custom-width position-relative mx-auto'>
+                    <div className='rounded shadow bar-statistic-vertical position-absolute'>
+                      <div className='custom-rounded' style={{ position: 'absolute', bottom: 0, height: '30%', width: '100%', backgroundColor: '#000' }}>
+                        <p className='text-white fw-normal fs-6 pt-2 text-center m-0'>2404</p>
+                        <p className='text-white fw-normal fs-7 text-center m-0'>articles</p>
+                      </div>
+                    </div>
+                  </div>
+                  <p className='text-muted fs-5 text-center pt-3'>0 - 20%</p>
+                </div>
+                <div id='bar2' className='px-2 flex-grow-1'>
+                  <div style={{ height: '300px' }} className='custom-width position-relative mx-auto'>
+                    <div className='rounded shadow bar-statistic-vertical position-absolute'>
+                      <div className='custom-rounded' style={{ position: 'absolute', bottom: 0, height: '30%', width: '100%', backgroundColor: '#000' }}>
+                        <p className='text-white fw-normal fs-6 pt-2 text-center m-0'>2404</p>
+                        <p className='text-white fw-normal fs-7 text-center m-0'>articles</p>
+                      </div>
+                    </div>
+                  </div>
+                  <p className='text-muted fs-5 text-center pt-3'>0 - 20%</p>
+                </div>
+                <div id='bar3' className='px-2 flex-grow-1'>
+                  <div style={{ height: '300px' }} className='custom-width position-relative mx-auto'>
+                    <div className='rounded shadow bar-statistic-vertical position-absolute'>
+                      <div className='custom-rounded' style={{ position: 'absolute', bottom: 0, height: '70%', width: '100%', backgroundColor: '#000' }}>
+                        <p className='text-white fw-normal fs-6 pt-2 text-center m-0'>2404</p>
+                        <p className='text-white fw-normal fs-7 text-center m-0'>articles</p>
+                      </div>
+                    </div>
+                  </div>
+                  <p className='text-muted fs-5 text-center pt-3'>0 - 20%</p>
+                </div>
+                <div id='bar4' className='px-2 flex-grow-1'>
+                  <div style={{ height: '300px' }} className='custom-width position-relative mx-auto'>
+                    <div className='rounded shadow bar-statistic-vertical position-absolute'>
+                      <div className='custom-rounded' style={{ position: 'absolute', bottom: 0, height: '30%', width: '100%', backgroundColor: '#000' }}>
+                        <p className='text-white fw-normal fs-6 pt-2 text-center m-0'>2404</p>
+                        <p className='text-white fw-normal fs-7 text-center m-0'>articles</p>
+                      </div>
+                    </div>
+                  </div>
+                  <p className='text-muted fs-5 text-center pt-3'>0 - 20%</p>
+                </div>
+                <div id='bar5' className='ps-2 flex-grow-1'>
+                  <div style={{ height: '300px' }} className='custom-width position-relative mx-auto'>
+                    <div className='rounded shadow bar-statistic-vertical position-absolute'>
+                      <div className='custom-rounded' style={{ position: 'absolute', bottom: 0, height: '30%', width: '100%', backgroundColor: '#000' }}>
+                        <p className='text-white fw-normal fs-6 pt-2 text-center m-0'>2404</p>
+                        <p className='text-white fw-normal fs-7 text-center m-0'>articles</p>
+                      </div>
+                    </div>
+                  </div>
+                  <p className='text-muted fs-5 text-center pt-3'>0 - 20%</p>
+                </div>
+              </div>
+              )
+            : (
+              <div />
+              )}
+        </div>
       </Container>
     </div>
   )
