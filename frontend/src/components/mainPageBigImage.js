4659dcd9898ae5d607918c36476d23558930a06e
Cristian Ciacu <d.c.ciacu@student.tudelft.nl>
2023-06-18 20:40:03 +0200

    fix checkstyle.

--- frontend/src/components/mainPageBigImage.js
+++ frontend/src/components/mainPageBigImage.js
@@ -59,11 +59,11 @@ export default function MainPageBigImage ({ description, projectName, imageUrl }
         <h2 className='title title_main_page'>{projectName}</h2>
         <p className='description-paragraph-main-page'>{description}</p>
       </div>
-      <a href="#ourMission">
-        <svg class="arrow">
-	        <path class="a1" d="M0 0 L30 22 L60 0"></path>
-	        <path class="a2" d="M0 20 L30 42 L60 20"></path>
-          <path class="a3" d="M0 40 L30 62 L60 40"></path>
+      <a href='#ourMission'>
+        <svg class='arrow'>
+          <path class='a1' d='M0 0 L30 22 L60 0' />
+          <path class='a2' d='M0 20 L30 42 L60 20' />
+          <path class='a3' d='M0 40 L30 62 L60 40' />
         </svg>
       </a>
     </div>
