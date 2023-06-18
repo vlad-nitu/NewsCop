6f0f8aacb8d7105ac169edb50f1ece75e083f112
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2023-06-18 18:51:25 +0200

    Solved the scrolling error

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -3,6 +3,7 @@ import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import { NavDropdown } from 'react-bootstrap'
 import { useState } from 'react'
+import { HashLink } from 'react-router-hash-link'
 
 /**
  * Function that renders the Navbar component.
@@ -15,10 +16,6 @@ import { useState } from 'react'
  * @returns React Component consisting in a navbar.
  */
 export default function NavbarComponent ({ name, mainPage }) {
-  const handleClick = () => {
-    window.scrollTo(0, 0)
-  }
-
   /**
      * Used for showing the dropdown menu on hover of the mouse.
      * See more here: https://stackoverflow.com/questions/60373789/react-bootstrap-dropdown-on-hover
@@ -37,13 +34,13 @@ export default function NavbarComponent ({ name, mainPage }) {
     <Navbar data-testid='navbar-component' bg='light' expand='lg' className='border-bottom-navbar'>
       <Container>
         {mainPage && <Navbar.Brand href='#home'>{name}</Navbar.Brand>}
-        {!mainPage && <Navbar.Brand href='/#home' data-testid='to-home-page' onClick={handleClick}>{name}</Navbar.Brand>}
+        {!mainPage && <Navbar.Brand href='/#home' data-testid='to-home-page'>{name}</Navbar.Brand>}
         <Navbar.Toggle aria-controls='content-on-the-right' />
         <Navbar.Collapse id='content-on-the-right' className='justify-content-end'>
           {/* ml-auto is used to align a particular element to the right side of its container. */}
           <Nav className='ml-auto'>
             {mainPage && <Nav.Link href='#ourMission'>About us</Nav.Link>}
-            {!mainPage && <Nav.Link href='/#ourMission' onClick={handleClick}>About us</Nav.Link>}
+            {!mainPage && <Nav.Link as={HashLink} to='/#ourMission'>About us</Nav.Link>}
             {mainPage && <Nav.Link href='#services'>Services</Nav.Link>}
             {!mainPage &&
               <NavDropdown
@@ -56,7 +53,7 @@ export default function NavbarComponent ({ name, mainPage }) {
                 <NavDropdown.Item role='option' href='/compareURLs'>Similarity Checker for two URLs</NavDropdown.Item>
               </NavDropdown>}
             {mainPage && <Nav.Link href='#statistics'>Statistics</Nav.Link>}
-            {!mainPage && <Nav.Link href='/#statistics' onClick={handleClick}>Statistics</Nav.Link>}
+            {!mainPage && <Nav.Link as={HashLink} to='/#statistics'>Statistics</Nav.Link>}
             <Nav.Link href='#footer'>Contact</Nav.Link>
             <Nav.Link href='/help'>Help</Nav.Link>
           </Nav>
